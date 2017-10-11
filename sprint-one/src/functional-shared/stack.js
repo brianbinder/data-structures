d0e8e232953751dfaa07f018e980e631888a1775
Brian Binder <bbinder912@gmail.com>
2017-10-11 15:43:49 -0700

    Complete stack functional-shared

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var newStack = {};
+  newStack.storage = {};
+  _.extend(newStack, stackMethods);
+  return newStack;
 };
 
 var stackMethods = {};
 
+stackMethods['push'] = function(value) {
+  var newKey = Object.keys(this.storage).length;
+  this.storage[newKey] = value;
+};
 
+stackMethods['pop'] = function() {
+  var poppedKey = Object.keys(this.storage).length - 1;
+  var poppedVal = this.storage[poppedKey];
+  delete this.storage[poppedKey];
+  return poppedVal;
+};
+
+stackMethods['size'] = function() {
+  return Object.keys(this.storage).length;
+};
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,21 +1,31 @@
 var Queue = function() {
+
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
-
+  var i = 0;
   someInstance.enqueue = function(value) {
-    var newKey = Object.keys(storage)[Object.keys(storage).length -1] + 1;
-    storage[newKey] = value;
+
+    storage['' + i] = value;
+    i++;
+    return storage;
   };
 
+  //return value with smallest key first
   someInstance.dequeue = function() {
-    var removedKey = Object.keys(storage)[0];
-    var removedVal = storage[removedKey];
-    delete storage[removedKey];
-    return removedVal;
+    //convert to numeric key, then grab all keys
+    var keysArr = Object.keys(storage).map(function(item) {
+      return parseInt(item);
+    });
+    //find smallest key
+    var minKey = Math.min.apply(null, keysArr);
+    //find the value with minKey;
+    var result = storage['' + minKey];
+    delete storage['' + minKey];
+    return result;
   };
 
   someInstance.size = function() {
@@ -23,4 +33,5 @@ var Queue = function() {
   };
 
   return someInstance;
+
 };
