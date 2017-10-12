07dc08e49d8f5b08721240c72b148af86d2f7460
Brian Binder <bbinder912@gmail.com>
2017-10-11 17:00:47 -0700

    Complete sprint part one

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,21 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
 
+Stack.prototype.push = function(value) {
+  var newKey = Object.keys(this.storage).length;
+  this.storage[newKey] = value;
+};
 
+Stack.prototype.pop = function() {
+  var popKey = Object.keys(this.storage).length - 1;
+  var popVal = this.storage[popKey];
+  delete this.storage[popKey];
+  return popVal;
+};
+
+Stack.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
