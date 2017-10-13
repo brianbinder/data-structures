fdf5d5d43bff11773ba69d5418c01ae5a668caa1
Brian Binder <bbinder912@gmail.com>
2017-10-12 19:18:30 -0700

    Complete set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,30 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this._storage.includes(item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.includes(item);
 };
 
 setPrototype.remove = function(item) {
+  if (this._storage.includes(item)) {
+    this._storage = this._storage.filter(function(element) {
+      return element !== item;
+    });
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+add: O(n); contains: O(n); remove: O(n);
  */
